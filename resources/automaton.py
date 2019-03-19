f5bd20aa5c269791e178cd93c3847d9009ec5a98
juanArias8 <juasda96@gmail.com>
2019-03-19 01:49:03 -0500

    dfa and nfa ready

---
+++ resources/automaton.py
@@ -0,0 +1,16 @@
+class Automaton(object):
+    def __init__(self, symbols: set, states: set, initial_state: str,
+                 acceptation_states: set, transitions: dict):
+
+        self.symbols = symbols
+        self.states = states
+        self.initial_state = initial_state
+        self.acceptation_states = acceptation_states
+        self.transitions = transitions
+
+    @staticmethod
+    def check_type(transitions: dict):
+        for transition in transitions.items():
+            for symbol in transition[1]:
+                if ',' in symbol:
+                    print(transition)
---
+++ resources/dfa.py
@@ -0,0 +1,115 @@
+import queue
+
+from resources.automaton import Automaton
+from resources.automaton_examples import odd_ones
+from resources.automaton_examples import strange_states
+from resources.exception import AutomatonException
+from resources.nfa import NFA
+
+
+class DFA(Automaton):
+
+    def __init__(self, symbols: set, states: set, initial_state: str,
+                 acceptation_states: set, transitions: dict):
+        super().__init__(symbols, states, initial_state, acceptation_states,
+                         transitions)
+
+        self.validate_dfa()
+        self.minify()
+
+    def validate_dfa(self) -> None:
+        self.check_initial_state()
+        self.check_acceptation_states()
+        self.check_transitions()
+
+    def check_initial_state(self) -> bool:
+        return self.initial_state in self.states
+
+    def check_acceptation_states(self) -> bool:
+        for state in self.acceptation_states:
+            if state not in self.states:
+                raise AutomatonException(f'{state} es un estado inválido')
+        return True
+
+    def check_transitions(self) -> bool:
+        for state in self.transitions.items():
+            self.check_transition_symbols(state[1])
+            self.check_transition_target_states(state[1])
+        return True
+
+    def check_transition_symbols(self, state: dict) -> None:
+        for symbol in state.keys():
+            if symbol not in self.symbols:
+                raise AutomatonException(f'{symbol} es un símbolo inválido')
+
+    def check_transition_target_states(self, state: dict) -> None:
+        for target_state in state.values():
+            if target_state not in self.states:
+                raise AutomatonException(
+                    f'{target_state} es un estado inválido')
+
+    def minify(self) -> None:
+        self.delete_strange_states()
+
+    def delete_strange_states(self) -> None:
+        valid_states = set()
+        states_to_check = queue.Queue()
+        states_checked = set()
+
+        states_to_check.put(self.initial_state)
+        while not states_to_check.empty():
+            state = states_to_check.get()
+            transition = self.transitions[state]
+
+            for symbol, target_state in transition.items():
+                if target_state not in states_checked:
+                    states_to_check.put(target_state)
+
+            states_checked.add(state)
+            valid_states.add(state)
+
+        self.states = valid_states
+        original_states = list(self.transitions.keys())
+
+        for state in original_states:
+            if state not in self.states:
+                del self.transitions[state]
+
+    def make_transition(self, state: str, symbol: str) -> str:
+        transition = self.transitions.get(state)
+        return transition.get(symbol)
+
+    def check_expression(self, expression: str):
+        state = self.initial_state
+        for item in expression:
+            state = self.make_transition(state, item)
+            print(state, " ==> ", item)
+        return self.check_match(state)
+
+    def check_match(self, state: str) -> bool:
+        return state in self.acceptation_states
+
+
+if __name__ == '__main__':
+    automaton = DFA(
+        strange_states.get('symbols'),
+        strange_states.get('states'),
+        strange_states.get('initial_state'),
+        strange_states.get('acceptation_state'),
+        strange_states.get('transitions')
+    )
+
+    automaton.minify()
+    print(automaton.transitions)
+
+if __name__ == '__main__':
+    automaton = NFA(
+        odd_ones.get('symbols'),
+        odd_ones.get('states'),
+        odd_ones.get('initial_state'),
+        odd_ones.get('acceptation_state'),
+        odd_ones.get('transitions')
+    )
+
+
+
