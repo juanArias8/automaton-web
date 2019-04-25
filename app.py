9b577850ad070957e656ece70b47968a7ab5ad0f
juanArias8 <juasda96@gmail.com>
2019-04-24 23:22:38 -0500

    Added errors handlers

--- app.py
+++ app.py
@@ -96,5 +96,15 @@ def match_string():
     return jsonify({'success': match})
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def exception_handler(e):
+    return render_template('500.html'), 500
+
+
 if __name__ == '__main__':
     app.run(debug=True)
